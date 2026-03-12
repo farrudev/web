f7cb1da9ba3f6c24ee879ea5c9b7a3993a734b17
farrudeveloper-cyber <farrudeveloper@gmail.com>
2026-03-12 18:37:41 -0300

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+
 const esBtn = document.getElementById("es")
 const enBtn = document.getElementById("en")
 
 esBtn.onclick = () => {
 
 document.getElementById("title").innerText = "Hola, soy FARRUKOOO"
-document.getElementById("subtitle").innerText = "Configurador medio"
+document.getElementById("subtitle").innerText = "CONFIGURADOR MEDIO"
 
 document.getElementById("desc").innerText =
 "Hola, soy farru, tengo 15 años y vivo en chile, me especializo en la configuracion de plugins principalmente YAML, Bots de discord y administrador de servidores, tengo 2 años de experiencia y me encuentro en constante aprendimiento, si me necesitas no dudes en contactarme."
@@ -14,9 +16,11 @@ document.getElementById("desc").innerText =
 enBtn.onclick = () => {
 
 document.getElementById("title").innerText = "Hi, I'm FARRUKOOO"
-document.getElementById("subtitle").innerText = "Plugin Configurator"
+document.getElementById("subtitle").innerText = "PLUGIN CONFIGURATOR"
 
 document.getElementById("desc").innerText =
 "Hi, I'm Farru, I'm 15 years old and I live in Chile. I specialize in plugin configuration mainly YAML, Discord bots and server administration. I have 2 years of experience and I'm constantly learning."
 
 }
+
+})
